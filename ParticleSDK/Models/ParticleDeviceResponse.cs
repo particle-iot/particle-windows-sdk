5285b91b13779f29ca9596b1e66c6656f0ed2bc6
Mark Topley-Drake <mtopleydrake@firemate.com>
2024-11-01 13:32:04 +0000

    Fixed DeviceVitals to match the latest format given by Particle API.

--- ParticleSDK/Models/ParticleDeviceResponse.cs
+++ ParticleSDK/Models/ParticleDeviceResponse.cs
@@ -19,8 +19,8 @@ namespace Particle.SDK.Models
         public string LastIPAddress { get; internal set; }
         [JsonProperty("last_heard")]
         public DateTime LastHeard { get; internal set; }
-        [JsonProperty("requires_deep_update")]
-        public bool RequiresDeepUpdate { get; internal set; }
+        [JsonProperty("last_handshake")]
+        public DateTime LastHandshake { get; internal set; }
         [JsonProperty("product_id")]
         public int ProductId { get; internal set; }
         [JsonProperty("online")]
@@ -29,19 +29,39 @@ namespace Particle.SDK.Models
         public int PlatformId { get; internal set; }
         [JsonProperty("cellular")]
         public bool Cellular { get; internal set; }
+        [JsonProperty("notes")]
+        public string Notes { get; internal set; }
+        [JsonProperty("firmware_updates_enabled")]
+        public bool FirmwareUpdatesEnabled { get; internal set; }
+        [JsonProperty("firmware_updates_forced")]
+        public bool FirmwareUpdatesForced { get; internal set; }
         [JsonProperty("status")]
         public string Status { get; internal set; }
-        [JsonProperty("last_iccid")]
-        public string LastICCID { get; internal set; }
+        [JsonProperty("serial_number")]
+        public string SerialNumber { get; internal set; }
+        [JsonProperty("iccid")]
+        public string ICCID { get; internal set; }
         [JsonProperty("imei")]
         public string IMEI { get; internal set; }
-        [JsonProperty("current_build_target")]
-        public string CurrentBuildTarget { get; internal set; }
+        [JsonProperty("mac_wifi")]
+        public string WiFiMAC { get; internal set; }
+        [JsonProperty("mobile_secret")]
+        public string MobileSecret { get; internal set; }
+        [JsonProperty("system_firmware_version")]
+        public string SystemFirmwareVersion { get; internal set; }
+        [JsonProperty("firmware_product_id")]
+        public int FirmwareProductId { get; internal set; }
+        [JsonProperty("firmware_version")]
+        public int FirmwareVersion { get; internal set; }
+        [JsonProperty("development")]
+        public bool Development { get; internal set; }
+        [JsonProperty("quarantined")]
+        public bool Quarantined { get; internal set; }
+        [JsonProperty("denied")]
+        public bool Denied { get; internal set; }
         [JsonProperty("variables")]
         public Dictionary<string, string> Variables { get; internal set; }
         [JsonProperty("functions")]
         public string[] Functions { get; internal set; }
-        [JsonProperty("cc3000_patch_version")]
-        public string CC3000PatchVersion { get; internal set; }
     }
 }
